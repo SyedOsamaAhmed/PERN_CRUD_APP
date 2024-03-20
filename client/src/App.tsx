22f0f2e1bab95f0570a37b372ac2a37ff42a19ee
Syed Osama Ahmed <batch17.026@gmail.com>
2024-03-20 16:22:22 +0500

    started frontend designing login page in progress

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import React,{Fragment} from 'react';
+
+
+//components:
+import Login from './components/login';
+
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+   <Login/>
+    </Fragment>
+  );
+}
+
+export default App;
